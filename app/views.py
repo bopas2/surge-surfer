4b1453d5f105dfde6cbdf161656346e4b7b3ee91
bopas2 <langthomas.tl@gmail.com>
2020-01-09 14:18:02 -0500

    Added text message capability

--- app/views.py
+++ app/views.py
@@ -1,22 +1,25 @@
 from flask import render_template, request
+from twilio.rest import Client
+from app import app
 
+import config
 import requests
 import json
 
-from app import app
-
-questionData = None
+client = Client(config.ACC_SID, config.AUTH_TOKEN)
+recieving_phone_number = '+12166780678'
+sending_phone_number = '+19516665806'
 
 @app.route('/', methods=["GET","POST"])
 def index():
-    # if request.method == "POST":
-    #     # if we want to submit the form
-    #     if request.form['button'] == 'Submit':
-    #     # if they clicked the random button instead
-    #     elif request.form['button'] == "Random":
+    if request.method == "POST":
+        if request.form['button'] == 'Submit':
+            message = client.messages \
+                .create(
+                    body='hello!',
+                    from_=sending_phone_number,
+                    to=recieving_phone_number
+                )
 
-    #     else:
-
-    # # GET requests
-    # else:
+            print(message.sid)
     return render_template("index.html", error = "")
